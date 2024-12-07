0e09cdaa28a61f554307b93fd75d402730a36cfa
Camilo Murcia Espinosa <147172021+CamiloMurcia28@users.noreply.github.com>
2024-12-06 23:52:27 -0500

    Update CorsConfig.java

--- src/main/java/edu/escuelaing/co/leotankcicos/config/CorsConfig.java
+++ src/main/java/edu/escuelaing/co/leotankcicos/config/CorsConfig.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class CorsConfig {
-    @Bean
+   // @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
