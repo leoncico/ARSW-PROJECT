3588b22fd5da0c860d20b39a678d4d5fba305df1
Camilo Murcia Espinosa <147172021+CamiloMurcia28@users.noreply.github.com>
2024-12-07 00:07:52 -0500

    Update CorsConfig.java

--- src/main/java/edu/escuelaing/co/leotankcicos/config/CorsConfig.java
+++ src/main/java/edu/escuelaing/co/leotankcicos/config/CorsConfig.java
@@ -5,18 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class CorsConfig {
-   // @Bean
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                    .allowedOrigins("https://frontarsw.z22.web.core.windows.net")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("*")
-                    .allowCredentials(true);
+                registry.addMapping("/**") // Aplica a todas las rutas
+                        .allowedOrigins("https://frontarsw.z22.web.core.windows.net") // Dominio del frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                        .allowedHeaders("*")
+                        .exposedHeaders("Authorization")// Permitir todos los encabezados
+                        .allowCredentials(true); // Permitir credenciales (cookies, tokens, etc.)
             }
         };
     }
