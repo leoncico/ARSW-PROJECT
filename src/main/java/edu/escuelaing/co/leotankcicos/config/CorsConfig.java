3a282fc6deb0568496b4bd522481ad5b9510fd9a
Camilo Murcia Espinosa <147172021+CamiloMurcia28@users.noreply.github.com>
2024-12-07 00:25:51 -0500

    Update CorsConfig.java

--- src/main/java/edu/escuelaing/co/leotankcicos/config/CorsConfig.java
+++ src/main/java/edu/escuelaing/co/leotankcicos/config/CorsConfig.java
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Aplica a todas las rutas
+                registry.addMapping("/api/tanks/**") // Aplica a todas las rutas
                         .allowedOrigins("https://frontarsw.z22.web.core.windows.net") // Dominio del frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
                         .allowedHeaders("*")
