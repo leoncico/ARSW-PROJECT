eab22c7c601f343a3223d250fc31ac16b35cc1da
Leonardo Piñeros <98127664+leoncico@users.noreply.github.com>
2024-12-07 01:06:26 -0500

    Update SecurityConfig.java

--- src/main/java/edu/escuelaing/co/leotankcicos/config/SecurityConfig.java
+++ src/main/java/edu/escuelaing/co/leotankcicos/config/SecurityConfig.java
@@ -3,9 +3,10 @@ package edu.escuelaing.co.leotankcicos.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
@@ -13,15 +14,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors() // Habilitar soporte para CORS
-            .and()
-            .csrf().disable() // Deshabilitar CSRF (opcional, según necesidad)
+            .csrf().disable()
             .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/api/tanks/login", "/api/tanks/username").permitAll() // Permitir estas rutas sin autenticación
-                .requestMatchers("/api/tanks/**").authenticated() // Rutas que requieren autenticación
-                .anyRequest().authenticated() // Cualquier otra solicitud requiere autenticación
+                .requestMatchers("/api/tanks/login", "/api/tanks/username").permitAll()
+                .requestMatchers("/api/tanks/**").authenticated()
+                .anyRequest().authenticated()
             )
-            .oauth2Login(); // Configuración de OAuth2
+            .oauth2Login()
+            .and();
         return http.build();
     }
 
@@ -42,4 +42,7 @@ public class SecurityConfig {
                 .clientName("Azure OAuth2")
                 .build();
     }
+
+    
 }
+
