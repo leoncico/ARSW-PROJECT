f5b4d98ec2427e75770301457245dab33d1f3d32
Camilo Murcia Espinosa <147172021+CamiloMurcia28@users.noreply.github.com>
2024-12-07 00:06:52 -0500

    Update SecurityConfig.java

--- src/main/java/edu/escuelaing/co/leotankcicos/config/SecurityConfig.java
+++ src/main/java/edu/escuelaing/co/leotankcicos/config/SecurityConfig.java
@@ -24,18 +24,4 @@ public class SecurityConfig {
             .oauth2Login(); // Configuración de OAuth2
         return http.build();
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Aplica a todas las rutas
-                        .allowedOrigins("https://frontarsw.z22.web.core.windows.net") // Dominio del frontend
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
-                        .allowedHeaders("*") // Permitir todos los encabezados
-                        .allowCredentials(true); // Permitir credenciales (cookies, tokens, etc.)
-            }
-        };
-    }
 }
