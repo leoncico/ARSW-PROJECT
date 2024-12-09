aac7c75612fb58d4bdf5686634199632f2529eed
leoncico <leoncico895@hotmail.com>
2024-12-09 16:41:26 -0500

    con cors config

---
+++ src/main/java/edu/escuelaing/co/leotankcicos/config/GlobalCorsConfig.java
@@ -0,0 +1,19 @@
+package edu.escuelaing.co.leotankcicos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Add global CORS configuration for HTTP endpoints (optional)
+        registry.addMapping("/**")
+                .allowedOrigins("https://frontarsw.z22.web.core.windows.net")  // Allowed origins for all HTTP endpoints
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
